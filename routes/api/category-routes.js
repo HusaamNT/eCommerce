2f92ce6f585680d6d470421f056bc88c457c77c7
Husaam NT <husnaeem@gmail.com>
2023-01-09 13:15:09 +0000

    added category get id, add and delete id routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,21 +12,43 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+ try{
+  const categoryData = await Category.findByPk(req.params.id);
+  if (!categoryData){
+    res.status(404).json({ message: 'Category not found'});
+  }
+  res.status(200).json(categoryData)
+ }catch(err){
+  res.status(500).json(err)
+ }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try{
+    const newCategory = await Category.create;
+    res.status(200).json(newCategory);
+  }catch(err){
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try{
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    res.status(200).json(categoryData);
+   }catch(err){
+    res.status(500).json(err);
+   }
 });
 
 module.exports = router;
