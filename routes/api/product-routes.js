f9fc8027092293857eeeaa90ef5195460cdc078c
Husaam NT <husnaeem@gmail.com>
2023-03-10 18:12:04 +0000

    added get all and get one product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,46 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-});
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [
+        Category,
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+}});
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+router.get('/:id', async (req, res) => {
+try {
+  const productData = await Product.findByPk(req.params.id, {
+    // JOIN with locations, using the Trip through table
+    include: [
+      Category,
+      { model: Tag, through: ProductTag }]
+  });
+
+  if (!productData) {
+    res.status(404).json({ message: 'No product found with this id!' });
+    return;
+  }
+
+  res.status(200).json(productData);
+} catch (err) {
+  res.status(500).json(err);
+}
+})
+;
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
