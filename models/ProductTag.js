6b8cedb0342801378196fa3996fb08edd864b380
Husaam NT <husnaeem@gmail.com>
2023-03-12 14:50:24 +0000

    added id in producttag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,11 +1,17 @@
-const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes, INTEGER } = require("sequelize");
 const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    product_tag: {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
       type: DataTypes.INTEGER,
       references: {
         model: "product",
